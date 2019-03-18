52b9e62ed1b18cb382aba0bb2b4dabfe867b967f
Siebecker <tsiebecker@gmx.de>
2019-03-18 05:31:07 +0100

    n-e-c and COOPTS

--- WPA-SEC/start_hashcat.sh
+++ WPA-SEC/start_hashcat.sh
@@ -2,11 +2,22 @@
 
 OUTDIR="database";
 ALL=false;
+NONCE_ERROR_CORRECTIONS=128;
+COOPTIONS="-a 0 -w 4 -D 1,2"
 
-while getopts ao: arguments; do
+while getopts ao:d:-: arguments; do
   case $arguments in
     a )  ALL=true ;;
     o )  OUTDIR="$OPTARG" ;;
+    - )  LONG_OPTARG="${OPTARG#*=}"
+         case $OPTARG in
+	   NONCE_ERROR_CORRECTIONS=?* )  NONCE_ERROR_CORRECTIONS="$LONG_OPTARG" ;;
+	   NONCE_ERROR_CORRECTIONS*   )  echo "No arg for --$OPTARG option" >&2; exit 2 ;;
+	   COOPTIONS=?* )  COOPTIONS="$LONG_OPTARG" ;;
+	   COOPTIONS*   )  echo "No arg for --$OPTARG option" >&2; exit 2 ;;
+           '' )        break ;; # "--" terminates argument processing
+           * )         echo "Illegal option --$OPTARG" >&2; exit 2 ;;
+         esac ;;
     \? ) exit 2 ;;  # getopts already reported the illegal option
   esac
 done
@@ -15,25 +26,17 @@ shift $((OPTIND-1)) # remove parsed options and args from $@ list
 if [ "${OUTDIR}" != "" ] ; then mkdir ${OUTDIR}; fi
 if [ "${OUTDIR: -1}" != "/" ]; then OUTDIR=${OUTDIR}"/"; fi
 
-hashcat -m 2501 --nonce-error-corrections=128 --remove --logfile-disable -a 0 -w 4 --potfile-path=${OUTDIR}hashcat.2501.pot --outfile-format=2 -o ${OUTDIR}foundhashcat.2501 ${OUTDIR}database_best.hccapx ${OUTDIR}"database_pmk(P)_list.db"
+hashcat -m 2501 --nonce-error-corrections=${NONCE_ERROR_CORRECTIONS} --remove --logfile-disable ${COOPTIONS} --potfile-path=${OUTDIR}hashcat.2501.pot --outfile-format=2 -o ${OUTDIR}foundhashcat.2501 ${OUTDIR}database_best.hccapx ${OUTDIR}"database_pmk(P)_list.db"
 
-sort -u ${OUTDIR}foundhashcat.2501 > ${OUTDIR}foundhashcat.2501.tmp
-cat ${OUTDIR}foundhashcat.2501.tmp > ${OUTDIR}foundhashcat.2501
-rm ${OUTDIR}foundhashcat.2501.tmp
+sort -uo ${OUTDIR}foundhashcat.2501 ${OUTDIR}foundhashcat.2501
 
-sort -u ${OUTDIR}hashcat.2501.pot > ${OUTDIR}hashcat.2501.pot.tmp
-cat ${OUTDIR}hashcat.2501.pot.tmp > ${OUTDIR}hashcat.2501.pot
-rm ${OUTDIR}hashcat.2501.pot.tmp
+sort -uo ${OUTDIR}hashcat.2501.pot ${OUTDIR}hashcat.2501.pot
 
-hashcat -m 2500 --nonce-error-corrections=128 --remove --logfile-disable -a 0 -w 4 --potfile-path=${OUTDIR}hashcat.2500.pot --outfile-format=2 -o ${OUTDIR}foundhashcat.2500 ${OUTDIR}database_best.hccapx ${OUTDIR}workinglist.db
+hashcat -m 2500 --nonce-error-corrections=${NONCE_ERROR_CORRECTIONS} --remove --logfile-disable ${COOPTIONS} --potfile-path=${OUTDIR}hashcat.2500.pot --outfile-format=2 -o ${OUTDIR}foundhashcat.2500 ${OUTDIR}database_best.hccapx ${OUTDIR}workinglist.db
 
-sort -u ${OUTDIR}foundhashcat.2500 > ${OUTDIR}foundhashcat.2500.tmp
-cat ${OUTDIR}foundhashcat.2500.tmp > ${OUTDIR}foundhashcat.2500
-rm ${OUTDIR}foundhashcat.2500.tmp
+sort -uo ${OUTDIR}foundhashcat.2500 ${OUTDIR}foundhashcat.2500
 
-sort -u ${OUTDIR}hashcat.2500.pot > ${OUTDIR}hashcat.2500.pot.tmp
-cat ${OUTDIR}hashcat.2500.pot.tmp > ${OUTDIR}hashcat.2500.pot
-rm ${OUTDIR}hashcat.2500.pot.tmp
+sort -uo ${OUTDIR}hashcat.2500.pot ${OUTDIR}hashcat.2500.pot
 
 hcxhashcattool -p ${OUTDIR}hashcat.2500.pot -P ${OUTDIR}"database_pmk(P)_list.db.tmp"
 
@@ -42,32 +45,24 @@ do
   echo ${p%%:*} >> ${OUTDIR}"database_pmk(P)_list.db"
 done < ${OUTDIR}"database_pmk(P)_list.db.tmp"
 
-sort -u ${OUTDIR}"database_pmk(P)_list.db" > ${OUTDIR}"database_pmk(P)_list.db.tmp"
-cat ${OUTDIR}"database_pmk(P)_list.db.tmp" > ${OUTDIR}"database_pmk(P)_list.db"
 rm ${OUTDIR}"database_pmk(P)_list.db.tmp"
+
+sort -uo ${OUTDIR}"database_pmk(P)_list.db" ${OUTDIR}"database_pmk(P)_list.db"
 
 if [ ${ALL} == true ]
 then
-	hashcat -m 2501 --nonce-error-corrections=128 --remove --logfile-disable -a 0 -w 4 --potfile-path=${OUTDIR}hashcat.2501.pot --outfile-format=2 -o ${OUTDIR}foundhashcat.2501 ${OUTDIR}database_all.hccapx ${OUTDIR}"database_pmk(P)_list.db"
+	hashcat -m 2501 --nonce-error-corrections=${NONCE_ERROR_CORRECTIONS} --remove --logfile-disable ${COOPTIONS} --potfile-path=${OUTDIR}hashcat.2501.pot --outfile-format=2 -o ${OUTDIR}foundhashcat.2501 ${OUTDIR}database_all.hccapx ${OUTDIR}"database_pmk(P)_list.db"
 	
-	sort -u ${OUTDIR}foundhashcat.2501 > ${OUTDIR}foundhashcat.2501.tmp
-	cat ${OUTDIR}foundhashcat.2501.tmp > ${OUTDIR}foundhashcat.2501
-	rm ${OUTDIR}foundhashcat.2501.tmp
-	
-	sort -u ${OUTDIR}hashcat.2501.pot > ${OUTDIR}hashcat.2501.pot.tmp
-	cat ${OUTDIR}hashcat.2501.pot.tmp > ${OUTDIR}hashcat.2501.pot
-	rm ${OUTDIR}hashcat.2501.pot.tmp
+	sort -uo ${OUTDIR}foundhashcat.2501 ${OUTDIR}foundhashcat.2501
 
-	hashcat -m 2500 --nonce-error-corrections=128 --remove --logfile-disable -a 0 -w 4 --potfile-path=${OUTDIR}hashcat.2500.pot --outfile-format=2 -o ${OUTDIR}foundhashcat.2500 ${OUTDIR}database_all.hccapx ${OUTDIR}workinglist.db
+	sort -uo ${OUTDIR}hashcat.2501.pot ${OUTDIR}hashcat.2501.pot
+
+	hashcat -m 2500 --nonce-error-corrections=${NONCE_ERROR_CORRECTIONS} --remove --logfile-disable ${COOPTIONS} --potfile-path=${OUTDIR}hashcat.2500.pot --outfile-format=2 -o ${OUTDIR}foundhashcat.2500 ${OUTDIR}database_all.hccapx ${OUTDIR}workinglist.db
 	
-	sort -u ${OUTDIR}foundhashcat.2500 > ${OUTDIR}foundhashcat.2500.tmp
-	cat ${OUTDIR}foundhashcat.2500.tmp > ${OUTDIR}foundhashcat.2500
-	rm ${OUTDIR}foundhashcat.2500.tmp
-	
-	sort -u ${OUTDIR}hashcat.2500.pot > ${OUTDIR}hashcat.2500.pot.tmp
-	cat ${OUTDIR}hashcat.2500.pot.tmp > ${OUTDIR}hashcat.2500.pot
-	rm ${OUTDIR}hashcat.2500.pot.tmp
-	
+	sort -uo ${OUTDIR}foundhashcat.2500 ${OUTDIR}foundhashcat.2500
+
+	sort -uo ${OUTDIR}hashcat.2500.pot ${OUTDIR}hashcat.2500.pot
+
 	hcxhashcattool -p ${OUTDIR}hashcat.2500.pot -P ${OUTDIR}"database_pmk(P)_list.db.tmp"
 	
 	while IFS="" read p || [ -n "$p" ]
@@ -75,7 +70,8 @@ then
 	  echo ${p%%:*} >> ${OUTDIR}"database_pmk(P)_list.db"
 	done < ${OUTDIR}"database_pmk(P)_list.db.tmp"
 	
-	sort -u ${OUTDIR}"database_pmk(P)_list.db" > ${OUTDIR}"database_pmk(P)_list.db.tmp"
-	cat ${OUTDIR}"database_pmk(P)_list.db.tmp" > ${OUTDIR}"database_pmk(P)_list.db"
 	rm ${OUTDIR}"database_pmk(P)_list.db.tmp"
+	
+	sort -uo ${OUTDIR}"database_pmk(P)_list.db" ${OUTDIR}"database_pmk(P)_list.db"
+	
 fi
